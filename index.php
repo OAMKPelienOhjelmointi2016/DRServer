fcaab847bee0af0c849a6906ecf7dc9049330803
unknown <esa.heinonen@DTFI-252.deetee.com>
2016-01-21 13:47:40 +0200

    Connection and query works in testmode / E

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__."/../database.php";
+/*require_once __DIR__."/../database.php";*/
 require_once __DIR__.'/../vendor/autoload.php';
 
 
@@ -13,11 +13,29 @@ $app->get('/hello', function ()
 
 $app->get('/users', function () 
 {
+	try
+	{
+	$conn_string = "mysql:host=mysql.hostinger.fi;dbname=u176618033_po16";
+	$db_Username = "u176618033_admin";
+	$db_Password = "q9uUHTUKU2";
+	$db = new PDO ($conn_string, $db_Username, $db_Password);
+	$db->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	print ("Connected\n");
+	}
+	catch (PDOException $e)
+	{
+	 print ("Cannot connect to server\n");
+	 print ("Error code: " . $e->getCode () . "\n");
+	 print ("Error message: " . $e->getMessage () . "\n");
+	}
+	
+	
 	/*header('Content-Type: application/json');*/
 	$stmt = $db->prepare("SELECT ID, firstName, lastName, playTime FROM users ORDER BY ID;");
 	$stmt->execute();
 	$results=$stmt->fetchAll(PDO::FETCH_ASSOC);
-	$json=json_encode(array('Reply' => $results));
+	var_dump ($results);
+	/*$json=json_encode(array('Reply' => $results));*/
 	/*echo $json; */
 	
     /*return $json;*/
