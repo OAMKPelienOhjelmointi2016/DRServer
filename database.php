0e8a592e5a69db3bbab5d72509f8dd0f5a785c21
unknown <esa.heinonen@DTFI-252.deetee.com>
2016-01-26 10:11:12 +0200

    Better connection file /E

--- database.php
+++ database.php
@@ -1,18 +1,37 @@
 <?php	
-	try
+	
+	function db_connect() 
 	{
-	$conn_string = "mysql:host=mysql.hostinger.fi;dbname=u176618033_po16";
-	$db_Username = "u176618033_admin";
-	$db_Password = "q9uUHTUKU2";
-	$db = new PDO ($conn_string, $db_Username, $db_Password);
-	$db->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	print ("Connected\n");
+		// Define connection as a static variable, to avoid connecting more than once 
+		static $connection;
+		
+		// Try and connect to the database, if a connection has not been established yet
+		if(!isset($connection)) 
+		{
+		try
+			{
+			$conn_string = "mysql:host=mysql.hostinger.fi;dbname=u176618033_po16";
+			$db_Username = "u176618033_admin";
+			$db_Password = "q9uUHTUKU2";
+			$connection = new PDO ($conn_string, $db_Username, $db_Password);
+			$connection->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+			/*print ("Connected\n");*/
+			}
+			catch (PDOException $e)
+			{
+			 print ("Cannot connect to server\n");
+			 print ("Error code: " . $e->getCode () . "\n");
+			 print ("Error message: " . $e->getMessage () . "\n");
+			}	
+		}
+		// Palautuarvon tarkistus
+		// If connection was not successful, handle the error
+
+    return $connection;
 	}
-	catch (PDOException $e)
-	{
-	 print ("Cannot connect to server\n");
-	 print ("Error code: " . $e->getCode () . "\n");
-	 print ("Error message: " . $e->getMessage () . "\n");
-	}
+	
+
+	
 	
 ?>
