1151ab256906890ce61a4de483b8953214cc1dcf
unknown <esa.heinonen@DTFI-252.deetee.com>
2016-02-02 09:48:23 +0200

    Add kills table insert and query /E

--- index.php
+++ index.php
@@ -140,5 +140,97 @@ $app->post('/shots', function ()
 }
 );
 
+$app->get('/kills', function () 
+{
+	$ID = $_GET['ID'];
+	$db = db_connect();
+	header('Content-Type: application/json');
+	$stmt = $db->prepare("SELECT kills, killed, hits FROM kills WHERE parentID = :ID;");
+	$stmt->bindParam(':ID', $ID);
+	$stmt -> execute();
+	$result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+		
+	$json=json_encode(array('Reply' => $result));
+	
+	return $json;
+}
+);
+
+$app->post('/kills', function () 
+{
+	try
+	{
+		$ID = $_POST['ID'];
+		
+		$kills = $_POST['kills'];
+		if (!isset ($kills))
+		{
+			$kills = 0;
+		}
+		$killed = $_POST['killed'];
+		if (!isset ($killed))
+		{
+			$killed = 0;
+		}
+		$hits = $_POST['hits'];
+		if (!isset ($hits))
+		{
+			$hits = 0;
+		}
+		
+		
+		$db = db_connect();
+		
+		$stmt = $db->prepare("SELECT ID FROM kills WHERE parentID = :ID;");
+		$stmt->bindParam(':ID', $ID);
+		$stmt->execute();
+		$result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		if ($result==NULL)
+		{
+			$stmt = $db->prepare("INSERT INTO kills (parentID, kills, killed, hits) VALUES (:ID, :kills, :killed, :hits);");
+			$stmt->bindParam(':ID', $ID);
+			$stmt->bindParam(':kills', $kills);
+			$stmt->bindParam(':killed', $killed);
+			$stmt->bindParam(':hits', $hits);
+			$stmt->execute();
+			$result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+		else 
+		{
+			if ($kills > 0)
+			{
+				$stmt = $db->prepare("UPDATE kills SET kills = kills + :kills WHERE parentID = :ID;");
+				$stmt->bindParam(':ID', $ID);
+				$stmt->bindParam(':kills', $kills);
+				$stmt->execute();
+			}
+			if ($killed > 0)
+			{
+				$stmt = $db->prepare("UPDATE kills SET killed = killed + :killed WHERE parentID = :ID;");
+				$stmt->bindParam(':ID', $ID);
+				$stmt->bindParam(':killed', $killed);
+				$stmt->execute();
+			}
+			if ($hits > 0)
+			{
+				$stmt = $db->prepare("UPDATE kills SET hits = hits + :hits WHERE parentID = :ID;");
+				$stmt->bindParam(':ID', $ID);
+				$stmt->bindParam(':hits', $hits);
+				$stmt->execute();
+			}
+						
+			/*$result=$stmt->fetchAll(PDO::FETCH_ASSOC);*/
+			return "kills/killed/hits created.";
+		}		
+	}	
+	catch(PDOException $e)
+    {
+		return $sql . "<br>" . $e->getMessage();	
+    }
+	
+}
+);
+
 
 $app->run();
