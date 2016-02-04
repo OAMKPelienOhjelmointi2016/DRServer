fda932ad1c602ad055c0df8eaee140aaa9b1e057
unknown <esa.heinonen@DTFI-252.deetee.com>
2016-02-04 11:33:55 +0200

    Added top10 shooters to stats page. /E

--- stats.php
+++ stats.php
@@ -24,13 +24,41 @@
 	echo "<th>Kills</th>";
 	echo "<th>Killed</th>";
 	echo "<th>Hits</th></tr><tr>";
-	foreach ($top10 as $arvo)
+	foreach ($top10 as $arvot)
 	{
-		foreach ($arvo as $arvo1)
-			echo "<td>$arvo1</td>";
+		foreach ($arvot as $arvot1)
+			echo "<td>$arvot1</td>";
 		echo "</tr>";
 	}
 	echo "</table>";	
+
+	$db = db_connect();
+	$stmt = $db->prepare("SELECT b.ID, b.nickName, b.firstName, b.lastName, a.parentID, a.fired
+							FROM shots AS a
+							LEFT JOIN users AS b ON a.ParentID = b.ID
+							ORDER BY a.fired DESC 
+							LIMIT 10;");
+	$stmt -> execute();							
+
+	echo "<h1>TOP10 Shooters</h1>";
+	while($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
+	{	
+		$rowarray2 = array($row['ID'], $row['nickName'], $row['firstName'], $row['lastName'], $row['fired']);
+		$shooters[] = $rowarray2;	
+	}
+	echo "<table border='1'><tr>";	
+	echo "<th>ID</th>";
+	echo "<th>Nick</th>";
+    echo "<th>First Name</th>";
+    echo "<th>Last Name</th>";
+	echo "<th>Shots fired</th></tr><tr>";
+	foreach ($shooters as $arvos)
+	{
+		foreach ($arvos as $arvos1)
+			echo "<td>$arvos1</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
 
 	$db = db_connect();
 	$stmt = $db->prepare("SELECT ID, nickName, firstName, lastName, playTime FROM users;");
@@ -39,8 +67,8 @@
 	echo "<h1>All Players</h1>";
 	while($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
 	{	
-		$rowarray = array($row['ID'], $row['nickName'], $row['firstName'], $row['lastName'], $row['playTime']);
-		$users[] = $rowarray;	
+		$rowarray3 = array($row['ID'], $row['nickName'], $row['firstName'], $row['lastName'], $row['playTime']);
+		$users[] = $rowarray3;	
 	}
 	echo "<table border='1'><tr>";	
 	echo "<th>ID</th>";
@@ -48,12 +76,11 @@
     echo "<th>First Name</th>";
     echo "<th>Last Name</th>";
 	echo "<th>Play Time</th></tr><tr>";
-	foreach ($users as $arvo)
+	foreach ($users as $arvou)
 	{
-		foreach ($arvo as $arvo1)
-			echo "<td>$arvo1</td>";
+		foreach ($arvou as $arvou1)
+			echo "<td>$arvou1</td>";
 		echo "</tr>";
 	}
 	echo "</table>";
-
 ?>
